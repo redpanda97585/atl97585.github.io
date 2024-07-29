e8be454eccbd409b1e2a90e625c5a307432adda0
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-07-29 12:38:35 -0600

    Update Change style in blog

--- script/02.js
+++ script/02.js
@@ -1,16 +1,16 @@
 const bodyElement = document.body;
-const mainContainer = document.querySelector(".main-container");
+// const mainContainer = document.querySelector(".main-container");
 mode = false;
 document.querySelector(".style-button").onclick = function () {
     if (mode === false) {
         bodyElement.style.background = "black";
         bodyElement.style.color = "white";
-        mainContainer.style.display = "flex";
+        // mainContainer.style.display = "flex";
         mode = true;
     } else {
         bodyElement.style.background = "white";
         bodyElement.style.color = "black";
-        mainContainer.style.display = "inline";
+        // mainContainer.style.display = "inline";
         mode = false;
     }
 }
